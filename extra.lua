fd646e64a4c114a2e7dfd1ff45ed8123b122704b
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2024-04-13 04:57:48 +0100

    document some more event parameters

--- extra.lua
+++ extra.lua
@@ -13,3 +13,39 @@
 ---@field name string Translated name of the item. <br> 'equipped:' is prepended if it is equipped. <br> 'hotbar:' is prepended if the item is in the hotbar, but not equipped.
 ---@field cat string Untranslated item category.
 ---@field weight number Weight of the stack.
+
+---@class MngInvItemData Data about a specific item stack in a player's inventory.
+---@field fullType string Full type of the item.
+---@field itemId integer ID of the item.
+---@field isEquip boolean Whether the item is equipped.
+---@field var number The used delta if the item is a drainable, or the item's condition if not.
+---@field count string The amount of items.
+---@field cat string The category of the item.
+---@field parrentId integer Item ID of the container this item is inside. -1 if it is not in a container
+---@field hasParrent boolean Whether the item has a parent.
+---@field container string Type of the container.
+---@field inInv boolean Always false?
+
+---@class MngInvItemTable Table containing details of another player's inventory.
+---@field capacityWeight number Total weight of items in the inventory.
+---@field maxWeight number Maximum weight the inventory can contain.
+---@field [integer] MngInvItemData Item stack data entries.
+
+-- TODO: idk what this really is lol
+---@class DBSchemaEntry
+---@field name string
+---@field type string
+
+---@alias PlayerDamageType
+---| '"POISON"' # Damage from poison.
+---| '"HUNGRY"' # Damage from hunger.
+---| '"SICK"' # Damage from sickness.
+---| '"BLEEDING"' # Damage from bleeding wounds.
+---| '"THIRST"' # Damage from thirst.
+---| '"HEAVYLOAD"' # Damage from over encumbrance.
+---| '"INFECTION"' # Damage from zombie infection.
+---| '"LOWWEIGHT"' # Damage from being severely underweight.
+---| '"FALLDOWN"' # Damage from falling.
+---| '"WEAPONHIT"' # Damage from being hit by weapons.
+---| '"CARHITDAMAGE"' # Damage from being hit by a car.
+---| '"CARCRASHDAMAGE"' # Damage from being inside a car when it crashes.
